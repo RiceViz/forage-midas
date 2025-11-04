bf7d42b4bf5e017df4090c30fed9e5a75a4a1b07
Tyran Rice <tyranricejr@gmail.com>
2025-11-04 14:28:48 -0500

    feat: adds incentive api integration

--- src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
+++ src/main/java/com/jpmc/midascore/entity/TransactionRecord.java
@@ -20,13 +20,17 @@ public class TransactionRecord {
     @Column(nullable = false)
     private float amount;
 
+    @Column(nullable = false)
+    private float incentive;
+
     protected TransactionRecord() {
     }
 
-    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount) {
+    public TransactionRecord(UserRecord sender, UserRecord recipient, float amount, float incentive) {
         this.sender = sender;
         this.recipient = recipient;
         this.amount = amount;
+        this.incentive = incentive;
     }
 
     public Long getId() {
@@ -57,10 +61,18 @@ public class TransactionRecord {
         this.amount = amount;
     }
 
+    public float getIncentive() {
+        return incentive;
+    }
+
+    public void setIncentive(float incentive) {
+        this.incentive = incentive;
+    }
+
     @Override
     public String toString() {
-        return String.format("TransactionRecord[id=%d, sender=%s, recipient=%s, amount=%f]", 
+        return String.format("TransactionRecord[id=%d, sender=%s, recipient=%s, amount=%f, incentive=%f]", 
                             id, sender != null ? sender.getName() : "null", 
-                            recipient != null ? recipient.getName() : "null", amount);
+                            recipient != null ? recipient.getName() : "null", amount, incentive);
     }
 }
